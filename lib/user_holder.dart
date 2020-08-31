387a3e01eb4361141935c1a27d2443a76d120ea6
Andrey Zherebtsov <drain295@mail.ru>
2020-08-31 16:42:45 +0200

    1

---
+++ lib/user_holder.dart
@@ -0,0 +1,15 @@
+import 'package:FlutterGalleryApp/models/user.dart';
+
+class UserHolder {
+  Map<String, User> users = {};
+
+  void registerUser(String name, String phone, String email) {
+    User user = User(name: name, phone: phone, email: email);
+
+    if (!users.containsKey(user.login)) {
+      users[user.login] = user;
+    } else {
+      throw Exception("A user with login allready exists!");
+    }
+  }
+}
