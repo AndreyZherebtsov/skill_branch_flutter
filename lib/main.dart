eca382cd2ea0f5dd13abf8759e00acff5ab907c6
Жеребцов Андрей Александрович <a.zherebtsov@scport.local>
2020-09-14 11:01:08 +0200

    1

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  // dsfsdfs
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
